afa4f26b99b194284328c3e9f29ffd3d37b7b079
Nic Bonetto <nick.bonetto@gmail.com>
2017-10-14 08:32:44 -0700

    Styled discover page. #2

--- client/components/discover-page/discover-page.jsx
+++ client/components/discover-page/discover-page.jsx
@@ -7,13 +7,18 @@ class DiscoverPage extends Component {
       <section className="discover-page container">
         <div className="row">
           <div className="col s12">
-            <header className="font-style">
-              <h4>Discover artists through an infinte chain of suggestions.</h4>
+            <header className="font-style center-align increase-margin-top">
+              <h5>Discover artists through an infinte chain of suggestions.</h5>
             </header>
           </div>
         </div>
         <div className="row">
-          <div className="col s12">
+          <div className="col s12 center-align">
+            <h5 className="font-style">Choose a recommened artist.</h5>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 artist-box">
             <CurrentArtist/>
           </div>
         </div>
